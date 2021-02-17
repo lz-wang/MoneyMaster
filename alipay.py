5990374a4abf7fdc95241ab5df802254eeaa0490
lzwang <zhuangwang82@gmail.com>
2021-02-17 16:13:59 +0800

    init

---
+++ alipay.py
@@ -0,0 +1,7 @@
+#  Copyright (c) lzwang 2020-2021, All Rights Reserved.
+#  File: alipay.py
+#  Project: MoneyMaster
+#  Author: Liangzhuang Wang
+#  Email: zhuangwang82@gmail.com
+#  Last modified: 2021/1/4 下午11:42
+
