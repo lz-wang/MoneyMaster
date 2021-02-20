b83b2fbd28f8fe4c7d0098010cd3a168fffdefc8
lzwang <zhuangwang82@gmail.com>
2021-02-21 01:19:51 +0800

    构建微信账单数据

--- mylog.py
+++ mylog.py
@@ -12,7 +12,8 @@ import datetime
 import os
 
 LOG_FOLDER = "/logs"
-LOG_FORMAT = "%(asctime)s - [%(levelname)s] - [%(filename)s, line.%(lineno)d]: %(message)s"
+LOG_FILE_NAME = str(datetime.date.today()) + '.log'
+LOG_FORMAT = "%(asctime)s [%(levelname)s] [%(filename)s, line.%(lineno)d]: %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
@@ -25,7 +26,7 @@ class Logger(object):
         'crit': logging.CRITICAL
     }  # 日志级别关系映射
 
-    def __init__(self, filename='MoneyMaster.log', level='debug'):
+    def __init__(self, filename=LOG_FILE_NAME, level='debug'):
         log_path = os.getcwd() + LOG_FOLDER
         if not os.path.exists(log_path):
             os.mkdir(log_path)
@@ -38,17 +39,22 @@ class Logger(object):
         # 设置屏幕输出
         sh = logging.StreamHandler()
         sh.setFormatter(self._log_fmt)
+        self.logger.addHandler(sh)
 
         # 设置文件输出
         # backupCount是文件的个数，如果超过这个个数，就会自动删除
         # interval是时间间隔，when是间隔的时间单位，单位有以下几种：
         # S 秒，M 分，H 小时，D 天，W 每星期（interval==0时代表星期一）
         # midnight 每天凌晨
-        th = handlers.TimedRotatingFileHandler(filename=log_file, when='midnight',
-                                               backupCount=30, encoding='utf-8')
-        th.setFormatter(self._log_fmt)
-        self.logger.addHandler(sh)
-        self.logger.addHandler(th)
+        # th = handlers.TimedRotatingFileHandler(filename=log_file, when='midnight',
+        #                                        backupCount=30, encoding='utf-8')
+        # th.setFormatter(self._log_fmt)
+        # self.logger.addHandler(th)
+
+        # 以日期为名称的log文件输出
+        fh = logging.FileHandler(filename=log_file, encoding='utf-8')
+        fh.setFormatter(self._log_fmt)
+        self.logger.addHandler(fh)
 
 
 if __name__ == '__main__':
--- wechat.py
+++ wechat.py
@@ -5,7 +5,9 @@
 #  Email: zhuangwang82@gmail.com
 #  Last modified: 2021/2/20 上午12:19
 import os
+import re
 import csv
+import datetime
 from mylog import Logger
 
 
@@ -13,17 +15,57 @@ class WechatManager:
     def __init__(self):
         self.log = Logger().logger
         self.csv_head = None
+        self.wechat_data = WechatPayData()
         pass
 
     def read_data(self, file_name):
         with open(file_name) as f:
             f_csv = csv.reader(f)
             # headers = next(f_csv)
+            row_idx = 0
             for row in f_csv:
-                if self.csv_head is not None:
+                if self.csv_head is None:
+                    self.find_statistics(row)
+                else:
                     self.log.info(row)
+                    self.wechat_data.data.append(row)
                 if row[0] == '----------------------微信支付账单明细列表--------------------':
                     self.csv_head = next(f_csv)
+                    self.wechat_data.data.append(self.csv_head)
+                row_idx += 1
+
+        pass
+
+    def find_statistics(self, row_data):
+        # 匹配方括号内的字符串
+        p1 = re.compile(r'[[](.*?)[]]', re.S)
+        # \d+ 匹配1次或者多次数字
+        # \.? 匹配小数点的，可能有，也可能没有
+        # \d* 匹配小数点之后的数字
+        p2 = re.compile(r'\d+\.?\d*')
+
+        for data in row_data:
+            if data == '':
+                continue
+            if data[0:4] == '微信昵称':
+                self.wechat_data.statistics['usr_name'] = re.findall(p1, data)[0]
+            elif data[0:4] == '起始时间':
+                self.wechat_data.statistics['start_time'] = re.findall(p1, data)[0]
+                self.wechat_data.statistics['end_time'] = re.findall(p1, data)[1]
+            elif data[0:4] == '导出时间':
+                self.wechat_data.statistics['export_time'] = re.findall(p1, data)[0]
+
+            if data[0] == '共':
+                self.wechat_data.statistics['trans_num'] = re.findall(p2, data)[0]
+            elif data[0:2] == '收入':
+                self.wechat_data.statistics['income_num'] = re.findall(p2, data)[0]
+                self.wechat_data.statistics['income_amount'] = re.findall(p2, data)[1]
+            elif data[0:2] == '支出':
+                self.wechat_data.statistics['expense_num'] = re.findall(p2, data)[0]
+                self.wechat_data.statistics['expense_amount'] = re.findall(p2, data)[1]
+            elif data[0:2] == '中性':
+                self.wechat_data.statistics['other_num'] = re.findall(p2, data)[0]
+                self.wechat_data.statistics['other_amount'] = re.findall(p2, data)[1]
 
     def write_data(self):
         pass
@@ -35,6 +77,24 @@ class WechatManager:
         pass
 
 
+class WechatPayData(object):
+    def __init__(self):
+        self.statistics = {
+            'usr_name': '',
+            'start_time': '',
+            'end_time': '',
+            'export_time': '',
+            'trans_num': 0,
+            'income_num': 0,
+            'income_amount': 0,
+            'expense_num': 0,
+            'expense_amount': 0,
+            'other_num': 0,
+            'other_amount': 0
+        }
+        self.data: list = []
+
+
 if __name__ == '__main__':
     print('')
     cur_path = os.getcwd()
@@ -42,4 +102,3 @@ if __name__ == '__main__':
     wm = WechatManager()
     wm.log.info(wechat_data)
     wm.read_data(wechat_data)
-
