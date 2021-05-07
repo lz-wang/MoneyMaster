0be847bf2c07b5fd1b2dd03a0fa963af173028f4
lzwang <zhuangwang82@gmail.com>
2021-05-08 00:26:06 +0800

    更新：微信支付数据统计项

--- model/WechatPayModel.py
+++ model/WechatPayModel.py
@@ -2,29 +2,29 @@
 #  File info: WechatPayModel.py in MoneyMaster (version 0.1)
 #  Author: Liangzhuang Wang
 #  Email: zhuangwang82@gmail.com
-#  Last modified: 2021/4/19 上午12:32
+#  Last modified: 2021/5/8 上午12:25
 
 class WechatPayData(object):
     def __init__(self):
         self.statistics = {
-            'usr_name': '',
-            'start_time': '',
-            'end_time': '',
-            'export_time': '',
-            'trans_num': 0,
-            'income_num': 0,
-            'income_amount': 0,
-            'expense_num': 0,
-            'expense_amount': 0,
-            'other_num': 0,
-            'other_amount': 0
+            'usr_name': '',       # 微信昵称
+            'start_time': '',     # 起始时间
+            'end_time': '',       # 终止时间
+            'export_type': '',    # 导出类型
+            'export_time': '',    # 导出时间
+            'trans_num': 0,       # 记录总数
+            'income_num': 0,      # 收入记录数
+            'income_amount': 0,   # 收入金额
+            'expense_num': 0,     # 支出记录数
+            'expense_amount': 0,  # 支出金额
+            'other_num': 0,       # 中性交易记录数
+            'other_amount': 0,    # 中性交易金额
         }
         self.data: list = []
 
 
 class WechatPayDB(object):
     def __init__(self):
-        self.db_name = 'money.db'
         self.table_name = 'wechat'
         self.table_attr = {
             'trans_time': 'DATETIME NOT NULL',      # 交易时间
@@ -37,7 +37,7 @@ class WechatPayDB(object):
             'status': 'VARCHAR(64) NOT NULL',       # 当前状态
             'trans_id': 'VARCHAR(64) NOT NULL',     # 交易单号
             'merchant_id': 'VARCHAR(64) NOT NULL',  # 商户单号
-            'reademe': 'VARCHAR(128) NOT NULL',     # 备注
+            'readme': 'VARCHAR(128) NOT NULL',      # 备注
         }
 
         self.test_data = {
@@ -51,6 +51,5 @@ class WechatPayDB(object):
             'status': '支付成功',
             'trans_id': 4200000782942012315746749070,
             'merchant_id': 1167798210211201231146827955,
-            'reademe': '/'
+            'readme': '/',
         }
-
